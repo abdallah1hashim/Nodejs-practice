eca4bd1872393b269960a107cb46ab154ecaeabe
Hashim <104575878+abdallah1hashim@users.noreply.github.com>
2023-12-23 03:18:39 +0000

    Update 7 files

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+app.use("/users", (req, res, send) => {
+  res.sendFile();
+});
+
+app.use("/", (req, res, send) => {
+  res.sendFile(path.join(__dir));
+});
+
+app.listen("3000");
